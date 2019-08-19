33933a4b686d1067f4e52dcde1d0909f0ba883ff
burhan116 <harianawalaburhanuddin@gmail.com>
2019-08-19 18:49:09 +0530

    button color update teacher registration Android 8 and 6 screen of figma

---
+++ app/src/main/java/com/example/feedbackapp/select_opt3.java
@@ -0,0 +1,14 @@
+package com.example.feedbackapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class select_opt3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_opt3);
+    }
+}
